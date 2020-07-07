85152508dd2b30c9bcbaa9a2934e6cd088cacdbc
awoken-coder <jfafandrade@gmail.com>
2020-07-07 17:38:12 +0100

    Added ability to remove, post, add content

--- index.js
+++ index.js
@@ -1,10 +1,85 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((req, res ) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Hello World')
+app.use(express.json())
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1
+    },
+    {
+        name: 'Ada Lovelace',
+        number: '39-44-5323523',
+        id: 2
+    },
+    {
+        name: 'Dan Abramov',
+        number: '12-43-234345',
+        id: 3
+    }, {
+        name: 'Mary Poppendieck',
+        number: '39-23-6423122',
+        id: 4
+    },
+]
+//Funtion Generate ID
+generateID = () => {
+    id = Math.ceil(Math.random()*10000000)
+    return id
+}
+
+//Method for info
+app.get('/info', (req, res) => {
+    const response = `<p>Phonebook has info for ${persons.length} people</p>
+<p>${Date()}</p>`
+    console.log(response)
+    res.send(response)
+})
+//Gets persons JSON
+app.get('/api/persons', (req, res) => {
+    console.log(persons)
+    res.json(persons)
+})
+//Gets info person.id
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(person => `${person.id}` === id)
+    console.log(person)
+    person ?
+        res.json(person)
+        : res.status(404).end()
+})
+//Delete person
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.filter(person => `${person.id}` !== id)
+    console.log(person)
+    
+    res.status(204).end()
+})
+//Add person
+app.post('/api/persons', (req, res)  =>  {
+    const person = req.body
+    if(!person.number || !person.name) {
+        const reason = person.number?'name':'number'
+        return res.status(400).json({error: `${reason} is missing`})
+    }
+    repeated = persons.filter((old)=>old.name ===person.name )
+    console.log(repeated)
+    if (repeated) {
+      return  res.status(400).json({error: 'name must be unique'})
+    }
+
+    person.id= generateID()
+    persons = persons.concat(person)
+    console.log('person',person)
+
+
+    res.json(person)
 })
 
 const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
